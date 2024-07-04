3f3f4965b7ad110d7c6f330e218fbcb1811f6f60
Santam Kumai <santtamk@gmail.com>
2024-07-04 21:49:44 +0530

    Hero section ready

--- medicalcentreslotbooking/src/components/heroSection/HeroSection.jsx
+++ medicalcentreslotbooking/src/components/heroSection/HeroSection.jsx
@@ -1,16 +1,22 @@
-
-
 const HeroSection = () => {
-  return (
-    <div className="flex flex-row gap-1">  
-      <div className="heroSection">
-        <div className="font-medium	text-3xl black">Skip the travel! Find Online</div>
-        <div className="text-6xl font-bold black">Medical <span className="text-6xl	font-bold sky">Centers</span></div>
-        <p>Connect instantly with a 24x7 specialist or choose to video visit a particular doctor.</p>
-        <button>Find Centers</button>
+  return ( 
+    <div className="flex justify-between p-4 gap-4 flex-col lg:flex-row md:py-4 md:px-5 lg:px-28">
+      <div className="heroSection flex flex-col gap-4">
+        <div className="font-medium	text-l black lg:font-medium lg:text-3xl	">
+          Skip the travel! Find Online
+        </div>
+        <div className="text-3xl font-bold lg:text-6xl lg:font-bold">
+          <span className="text-black pr-4">Medical</span>
+          <span className="text-sky">Centers</span>
+        </div>
+        <p className="text-light-grey font-normal	text-l lg:text-xl	">
+          Connect instantly with a 24x7 specialist or choose to video visit a
+          particular doctor.
+        </p>
+        <button className="text-white bg-sky rounded-lg w-44 h-12 text-base font-medium">Find Centers</button>
       </div>
-      <div>
-        <img src="./heroImage.png" alt="HeroImage" />
+      <div className="flex justify-center items-center md:justify-items-center	md:items-center	">
+        <img src="./heroImage.png" alt="HeroImage" className="object-contain"/>
       </div>
     </div>
   );
--- medicalcentreslotbooking/tailwind.config.js
+++ medicalcentreslotbooking/tailwind.config.js
@@ -16,7 +16,6 @@ export default {
       'navy': '#1B3C74',
       'light-grey': '#5C6169',
       'offWhite': 'linear-gradient(81deg, #E7F0FF 9.01%, rgba(232, 241, 255, 0.47) 89.11%)',
-
     },
   },
   plugins: [],
