985db8a2950e3fce8d1bda6061772790350a451c
Santam Kumai <santtamk@gmail.com>
2024-07-12 14:48:40 +0530

    minor section completed

--- medicalcentreslotbooking/src/components/families/Families.jsx
+++ medicalcentreslotbooking/src/components/families/Families.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+
 
 const Families = () => {
   return (
-    <div>Families</div>
+    <div className="bg-white-off ">
+        <div className="m-8 md:p-10 flex flex-col md:flex-row lg:mx28">
+            <div className="lg:w-6/12 md:w-6/12 flex flex-col items-left justify-center gap-5">
+                <div className="text-sky text-base font-semibold">
+                CARING FOR THE HEALTH OF YOU AND YOUR FAMILY.
+                </div>
+                <div className="text-navy font-semibold text-5xl">
+                Our Families
+                </div>
+                <p className="text-light-grey font-medium	text-base">
+                We will work with you to develop individualised care plans, including management of chronic diseases. If we cannot assist, we can provide referrals or advice about the type of practitioner you require. We treat all enquiries sensitively and in the strictest confidence.
+                </p>
+            </div>
+            <div className="pt-4 flex items-center justify-center lg:w-6/12 md:w-6/12">
+                <img src="./4images.png" alt="4images" />
+            </div>
+
+        </div>
+    </div>
   )
 }
 
